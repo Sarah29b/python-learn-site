c7f5ae4bdd331ffbb2e7e6eb67c124253c007a00
sbenslimane <sarah@SarahBEN29.localdomain>
2025-07-10 07:30:21 +0200

    site du server 6

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 const authRoutes = require('./server/routes/auth');
 app.use('/auth', authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
