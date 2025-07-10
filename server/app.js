7ad41faf2272321054008637b92a637dbe2c30fd
sbenslimane <sarah@SarahBEN29.localdomain>
2025-07-10 07:21:15 +0200

    site du server 5

--- server/app.js
+++ server/app.js
@@ -9,10 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'python-learn-site','public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'python-learn-site', 'public', 'index.html'));
 });
 
 const authRoutes = require('./routes/auth'); //On importe les routes d’authentification 
