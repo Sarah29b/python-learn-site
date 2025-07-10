27385533879f7f48566fc37bc5a357738e79dd3a
sbenslimane <sarah@SarahBEN29.localdomain>
2025-07-10 06:55:15 +0200

    site du server 2

--- server/app.js
+++ server/app.js
@@ -9,15 +9,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static('../public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 const authRoutes = require('./routes/auth'); //On importe les routes d’authentification 
 //(register/login), définies dans le fichier routes/auth.js.
 app.use('/auth', authRoutes); //On connecte le routeur authRoutes au chemin /auth.
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-});
 
 const PORT =  3000; //On récupère le port à utiliser à partir du fichier .env.
 //S’il n’est pas défini, on utilise 3000 par défaut.
