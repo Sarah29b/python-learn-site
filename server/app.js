1cdd06203c1fe4d3564f9d4ca055b053b7c225f1
sbenslimane <sarah@SarahBEN29.localdomain>
2025-07-10 06:45:22 +0200

    site du server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors'); // permet au backend d’accepter les requêtes
 // provenant d’un autre domaine (comme frontend)
+const path = require('path');
 require('dotenv').config(); //On charge les variables d’environnement
 // stockées dans un fichier .env (ex : le port, l’URL de la base de données).
 
@@ -8,16 +9,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 const authRoutes = require('./routes/auth'); //On importe les routes d’authentification 
 //(register/login), définies dans le fichier routes/auth.js.
 app.use('/auth', authRoutes); //On connecte le routeur authRoutes au chemin /auth.
 
 const PORT =  3000; //On récupère le port à utiliser à partir du fichier .env.
 //S’il n’est pas défini, on utilise 3000 par défaut.
-
-app.get('/', (req, res) => {
-  res.send('Bienvenue sur Python Learn Site 🚀');
-});
 
 
 app.listen(PORT, '0.0.0.0', () => {
