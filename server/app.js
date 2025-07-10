a94c1b0cd6e3141ab7da4efe59a7a65c6ad23ef4
sbenslimane <sarah@SarahBEN29.localdomain>
2025-07-10 07:24:55 +0200

    site du server 5

--- server/app.js
+++ server/app.js
@@ -1,28 +1,24 @@
-const express = require('express');
-const cors = require('cors'); // permet au backend d’accepter les requêtes
-// provenant d’un autre domaine (comme frontend)
 const path = require('path');
-require('dotenv').config(); //On charge les variables d’environnement
-// stockées dans un fichier .env (ex : le port, l’URL de la base de données).
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'python-learn-site','public')));
+// CORRECTION ICI : utiliser chemin absolu correct
+const publicPath = path.join(process.cwd(), 'public');
+app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'python-learn-site', 'public', 'index.html'));
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-const authRoutes = require('./routes/auth'); //On importe les routes d’authentification 
-//(register/login), définies dans le fichier routes/auth.js.
-app.use('/auth', authRoutes); //On connecte le routeur authRoutes au chemin /auth.
+const authRoutes = require('./routes/auth');
+app.use('/auth', authRoutes);
 
-const PORT =  3000; //On récupère le port à utiliser à partir du fichier .env.
-//S’il n’est pas défini, on utilise 3000 par défaut.
-
-
+const PORT =  3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
